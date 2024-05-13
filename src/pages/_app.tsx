b85698f0e7919e360d4a4a7cf42d3ae21f08120b
PavlosDefoort <psnpenguin2@gmail.com>
2024-05-12 21:38:48 -0400

    Initial commit from Create Next App
    Co-authored-by: Christian Zhou <christianzhou1@users.noreply.github.com>

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,17 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { Jacquard_24 } from "next/font/google";
+
+const jackquard = Jacquard_24({
+  weight: ["400"],
+  style: ["normal"],
+  subsets: ["latin"],
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <main className={`${jackquard.className}`}>
+      <Component {...pageProps} />
+    </main>
+  );
 }
