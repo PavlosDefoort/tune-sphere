a83440d86e2824c9f5bcb98595a1e1932bfbdf41
PavlosDefoort <psnpenguin2@gmail.com>
2024-07-31 22:01:48 -0400

    Fixed

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   env: {
-    CLIENT_ID: "",
+    CLIENT_ID: "21ab1dc112ba47e1ad7657b408d0c3cf",
     CLIENT_SECRET: "",
     REDIRECT_URI: "http://localhost:3000/api/callback",
     JWT_SECRET: "supersecret",
