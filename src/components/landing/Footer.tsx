0e4016792ecb0a0060c4d603bee3a80f3f87205f
christianzhou1 <christianzhiruizhou@outlook.com>
2024-08-01 01:43:51 -0400

    revert footer color

--- src/components/landing/Footer.tsx
+++ src/components/landing/Footer.tsx
@@ -2,7 +2,7 @@ import { GitHub, LinkedIn } from "@mui/icons-material";
 
 const Footer: React.FC = () => {
   return (
-    <footer className="dark:text-white text-black body-font bg-backgroundColor-dark dark:bg-slate-600">
+    <footer className="dark:text-white text-black body-font bg-backgroundColor-dark dark:bg-backgroundColor-dark">
       <div className="container px-5 py-8 mx-auto flex items-center sm:flex-row flex-col">
         <a className="flex title-font font-medium items-center md:justify-start justify-center text-gray-900">
           <svg
